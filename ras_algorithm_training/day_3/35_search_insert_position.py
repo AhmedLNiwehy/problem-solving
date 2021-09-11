3f13e2c8fad5e171f224036fccf2bbe0b6f1c352
Ahmed Mahmoud <Ahmed1020100200@gmail.com>
2021-09-11 20:35:44 +0200

    day-3, task-2, Search Insert Position

---
+++ ras_algorithm_training/day_3/35_search_insert_position.py
@@ -0,0 +1,42 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+"""
+
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (high + low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        if target > nums[len(nums)-1]:
+            return len(nums)
+        elif target > nums[mid]:
+            return mid+1
+        elif target < nums[mid]:
+            return mid
+        else:
+            return 0
+
+
+S = Solution()
+print(S.searchInsert([1, 3, 5, 6], 2))
