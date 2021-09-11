bfbb34595d44b83188d84c606e9d85873c9a656a
Ahmed Mahmoud <Ahmed1020100200@gmail.com>
2021-09-12 00:02:09 +0200

    day-3, task-3, Majority Element

---
+++ ras_algorithm_training/day_3/169_Majority_Element.py
@@ -0,0 +1,22 @@
+﻿""""
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that
+the majority element always exists in the array.
+
+Example:
+Input: nums = [3,2,3]
+Output: 3
+"""
+
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        mid = len(nums)//2
+        return nums[mid]
+
+
+S = Solution()
+print(S.majorityElement([1, 2, 1]))
