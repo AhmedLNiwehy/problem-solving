964e54b521b99d1ac14c92c6a38712e7967897f3
Ahmed Mahmoud <Ahmed1020100200@gmail.com>
2021-09-24 04:12:20 +0200

    day_8, task_8, Reverse Linked List

---
+++ ras_algorithm_training/day_8/206_Reverse_Linked_List.py
@@ -0,0 +1,23 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+"""
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            head.next, head, prev = prev, head.next, head
+        return prev
